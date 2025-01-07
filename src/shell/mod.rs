b1e8cd6fb008549bcb4ae44f0b8ec3ce0af7eafd
falehalnayef <alnayef.faleh@gmail.com>
2025-01-07 23:26:38 +0300

    feat: adding support for single and double quotes in string

--- src/shell/mod.rs
+++ src/shell/mod.rs
@@ -41,8 +41,43 @@ impl Shell {
         if input.is_empty() {
             return None;
         }
-
-        let tokens = input.split_whitespace().collect::<Vec<&str>>();
+        let mut tokens = Vec::new();
+        let mut start = None;
+        let mut in_quotes = false;
+        let mut quote_char = '\0';
+    
+        for (i, c) in input.char_indices() {
+            match c {
+                '\'' | '"' if !in_quotes => {
+                    in_quotes = true;
+                    quote_char = c;
+                    start = Some(i + 1);
+                }
+                '\'' | '"' if in_quotes && c == quote_char => {
+                    in_quotes = false;
+                    if let Some(start_index) = start {
+                        tokens.push(&input[start_index..i]);
+                    }
+                    start = None;
+                }
+                ' ' if !in_quotes => {
+                    if let Some(start_index) = start {
+                        tokens.push(&input[start_index..i]);
+                    }
+                    start = None;
+                }
+                _ if start.is_none() && !in_quotes => {
+                    start = Some(i);
+                }
+                _ => {}
+            }
+        }
+    
+        // Add the last token if it exists
+        if let Some(start_index) = start {
+            tokens.push(&input[start_index..]);
+        }
+    
         Some(Command::from_tokens(tokens))
-    }
+}
 }
